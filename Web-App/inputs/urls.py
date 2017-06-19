c5c3fe89697d0661d5c93994166b4f0194e9a4b0
Ben Latham <bl15686@my.bristol.ac.uk>
2017-06-19 12:08:06 +0100

    fixing migrations

--- Web-App/inputs/urls.py
+++ Web-App/inputs/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<page>((intro)|(financial)|(assets)|(land)|(dairy)|(cropping)|(forage)|(summary)))?$', views.inputs, name='inputs'),
-    url(r'^enterprises$', views.enterprises, name='enterprises'),
-    url(r'^enterprises/success/$', views.enterprises_submitted, name='enterprises submitted'),
     url(r'^test$', views.formset_test, name='test')
 ]
 
--- Web-App/inputs/views.py
+++ Web-App/inputs/views.py
@@ -1,37 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from.forms import EnterpriseForm, FormsetFormFormset
+from.forms import FormsetFormFormset
 
-from simulation.models import Enterprise, SimulationYear
 @login_required
 def inputs(request, page):
     if not page:
       page = "intro"
     return HttpResponse(render(request,'inputs/'+page+'.html'))
 
-def enterprises(request):
-    # if this is a POST request we need to process the form data
-
-    year = SimulationYear(year_ending=1)
-    year.save()
-    enterprise= Enterprise(year=year)
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = EnterpriseForm(request.POST, instance=enterprise)
-        # check whether it's valid:
-        if form.is_valid():
-            form.save()
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('enterprises/success/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = EnterpriseForm()
-
-    return render(request, 'inputs/enterprises.html', {'form': form})
 
 def enterprises_submitted(request):
     return HttpResponse(render(request, "inputs/enterprises_submitted.html"))
